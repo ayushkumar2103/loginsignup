4296be24b42cab198a8ee8d973f23065a46ca4a4
ayushkumar2103 <87165537+ayushkumar2103@users.noreply.github.com>
2024-02-15 00:43:08 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBghbN9jvUXaRRUtVMnVEH17o50ym6mvP8",
+    authDomain: "loginassignment-d4ac4.firebaseapp.com",
+    databaseURL: "https://loginassignment-d4ac4-default-rtdb.firebaseio.com",
+    projectId: "loginassignment-d4ac4",
+    storageBucket: "loginassignment-d4ac4.appspot.com",
+    messagingSenderId: "834712102301",
+    appId: "1:834712102301:web:0aa2ea3cb618036dbdfb74",
+    measurementId: "G-KLR75Y8JFM"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
